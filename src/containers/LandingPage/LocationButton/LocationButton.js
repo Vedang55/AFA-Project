c2dd0cf7931a52f4a887ab2ee9d6ca9e00d57393
Vedang55 <Vedang55.vd@gmail.com>
2020-01-27 16:08:35 +0530

    styling fixes

--- src/containers/LandingPage/LocationButton/LocationButton.js
+++ src/containers/LandingPage/LocationButton/LocationButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 import classes from './LocationButton.module.css';
 
 const LocationButton = (props) => {
-    let classArr = [classes.Container, classes.NotSelected];
+    let classArr = [classes.Container];
     if (props.selected==='true'){
         classArr[1] = classes.Selected;
     }
     return (
         <div className={classArr.join(' ')} onClick={props.clicked}>
             <p>{props.place}</p>
-            <hr/>
+            <hr className={props.selected==='true'? classes.Selected : undefined}/>
         </div>
     );
 }
