6cbc232ac94226aafd4ba44094ed2d1102636f1b
Vedang55 <Vedang55.vd@gmail.com>
2020-02-06 13:58:14 +0530

    scroll to top react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route, Router } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import LandingPage from './containers/LandingPage/LandingPage';
 import Gallery from './containers/Gallery/Gallery';
 import Equipment from './containers/Equipment/Equipment';
 import AboutUs from './containers/AboutUs/AboutUs';
 import Clubs from './containers/Clubs/Clubs';
 import Publications from './containers/Publications/Publications'
+import ScrollToTop from "./components/ScrollToTop";
+
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={LandingPage}/>
-      <Route path="/gallery" component={Gallery}/>
-      <Route path="/equipment" component={Equipment}/>
-      <Route path="/aboutus" component={AboutUs}/>
-      <Route path="/clubs" component={Clubs}/>
-      <Route path="/publications" component={Publications}/>
-
+      <ScrollToTop>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/gallery" component={Gallery} />
+        <Route path="/equipment" component={Equipment} />
+        <Route path="/aboutus" component={AboutUs} />
+        <Route path="/clubs" component={Clubs} />
+        <Route path="/publications" component={Publications} />
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
