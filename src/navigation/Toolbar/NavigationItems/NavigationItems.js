4b5505a9c17593c819c7b1c4f804ed07779896df
Vedang55 <Vedang55.vd@gmail.com>
2020-02-08 13:13:55 +0530

    nav link order

--- src/navigation/Toolbar/NavigationItems/NavigationItems.js
+++ src/navigation/Toolbar/NavigationItems/NavigationItems.js
@@ -6,10 +6,10 @@ const NavigationItems = (props) => {
 
     return (
         <nav className={classes.Nav} style={{...props.style}} >
-            <NavLink to="/equipment" activeClassName={classes.active}>Equipment</NavLink>
-            <NavLink to="/gallery" activeClassName={classes.active}>Gallery</NavLink>
             <NavLink to="/clubs" activeClassName={classes.active}>Clubs</NavLink>
             <NavLink to="/publications" activeClassName={classes.active}>Publications</NavLink>
+            <NavLink to="/equipment" activeClassName={classes.active}>Equipment</NavLink>
+            <NavLink to="/gallery" activeClassName={classes.active}>Gallery</NavLink>
             <NavLink to="/aboutus" activeClassName={classes.active}>About Us</NavLink>
         </nav>
     );
